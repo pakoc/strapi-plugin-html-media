8bca1ba4f34f77d9e6bb8a40467d352ad5452aea
Pakoc <kochnevpav@mail.ru>
2025-03-21 16:16:08 +0300

    0.0.8 - add upload route

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -1,30 +1,49 @@
 import pluginId from "../../admin/src/pluginId";
 
-export default [
-  {
-    method: "GET",
-    path: "/:uid",
-    handler: "htmlMediaController.findOne",
-    config: {
-      auth: false,
-      policies: [],
-    },
+export default {
+  admin: {
+    type: "admin",
+    routes: [
+      {
+        method: "GET",
+        path: "/:uid",
+        handler: "htmlMediaController.findOne",
+        config: {
+          auth: false,
+          policies: [],
+        },
+      },
+      {
+        method: "POST",
+        path: "/",
+        handler: "htmlMediaController.upload",
+        config: {
+          policies: [],
+          middlewares: [`plugin::${pluginId}.uploadMiddleware`],
+        },
+      },
+      {
+        method: "DELETE",
+        path: "/:uid",
+        handler: "htmlMediaController.delete",
+        config: {
+          policies: [],
+        },
+      },
+    ],
   },
-  {
-    method: "POST",
-    path: "/",
-    handler: "htmlMediaController.upload",
-    config: {
-      policies: [],
-      middlewares: [`plugin::${pluginId}.uploadMiddleware`],
-    },
+  "content-api": {
+    type: "content-api",
+    routes: [
+      {
+        method: "POST",
+        path: "/",
+        handler: "htmlMediaController.upload",
+        config: {
+          policies: [],
+          middlewares: [`plugin::${pluginId}.uploadMiddleware`],
+        },
+      },
+    ],
   },
-  {
-    method: "DELETE",
-    path: "/:uid",
-    handler: "htmlMediaController.delete",
-    config: {
-      policies: [],
-    },
-  },
-];
+};
